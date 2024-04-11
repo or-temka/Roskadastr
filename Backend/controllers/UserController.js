9ec75ff0268a39ffd8040f9dfcb67efc6afc7dda
artem schegortzov <tema.chegortzov@mail.ru>
2024-04-11 15:02:33 +0300

    added user: login & getInfo
    also -> new userController

---
+++ Backend/controllers/UserController.js
@@ -0,0 +1,115 @@
+import { TOKEN_KEY } from '../PASSWORDS.js'
+
+import { validationResult } from 'express-validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+import { serverError, serverLog } from '../utils/serverLog.js'
+import UserModel from '../models/User.js'
+
+
+
+export const reg = async (req, res) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
+
+    // hashing password
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const doc = new UserModel({
+      login: req.body.login,
+      passwordHash: hashedPassword,
+      name: req.body.name,
+      surname: req.body.surname,
+      dateOfBirth: req.body.dateOfBirth,
+      city: req.body.city,
+      branch: req.body.branch,
+    })
+
+    const user = await doc.save()
+
+    serverLog(`Успешно создан пользователь с логином ${user.login}`)
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      TOKEN_KEY
+    )
+
+    res.json({ token: token })
+  } catch (err) {
+    serverError(err)
+    if (err?.keyPattern?.login === 1) {
+      return res.status(409).json({
+        errorMsg: 'Логин уже занят',
+      })
+    }
+    res.status(500).json({
+      errorMsg: 'Произошла ошибка во время регистрации',
+    })
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ login: req.body.login })
+
+    if (!user) {
+      return res.status(404).json({
+        errorMsg: 'Неверный логин или пароль',
+      })
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    )
+
+    if (!isValidPass) {
+      return res.status(404).json({
+        errorMsg: 'Неверный логин или пароль',
+      })
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      TOKEN_KEY
+    )
+
+    res.json({ token: token })
+  } catch (err) {
+    serverError(err)
+    res.status(500).json({
+      errorMsg: 'Произошла ошибка входа в аккаунт',
+    })
+  }
+}
+
+export const getMeInfo = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId)
+
+    if (!user) {
+      return res.status(404).json({
+        errorMsg: 'Пользователь не найден',
+      })
+    }
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json(userData)
+  } catch (error) {
+    serverError(err)
+    res.status(500).json({
+      errorMsg: 'Ошибка получения данных о пользователе',
+    })
+  }
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,19 @@
+import { DB_LOGIN, DB_PASSWORD, TOKEN_KEY } from './PASSWORDS.js' //You should create your file with datas
+
 import express from 'express'
-import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
-import { validationResult } from 'express-validator'
 
 import { serverError, serverLog } from './utils/serverLog.js'
-
 import { registerValidation } from './validations/auth.js'
-
-import UserModel from './models/User.js'
-
-import { DB_LOGIN, DB_PASSWORD, TOKEN_KEY } from './PASSWORDS.js' //You should create your file with datas
+import checkAuth from './utils/checkAuth.js'
+import * as UserController from './controllers/UserController.js'
 
 mongoose
   .connect(
     `mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@cluster0.rouuumh.mongodb.net/roskadastr?retryWrites=true&w=majority&appName=Cluster0`
   )
-  .then(() => console.log('connected to DB'))
-  .catch((err) => console.log('connected error to DB!!! Error: ', err))
+  .then(() => serverLog('connected to DB'))
+  .catch((err) => serverError('connected error to DB!!! Error: ' + err))
 
 const app = express()
 const PORT = 4000
@@ -26,54 +22,11 @@ app.use(express.json())
 
 //#region User
 // Registration user (sign up)
-app.post('/auth/reg', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array())
-    }
-
-    // hashing password
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-
-    const doc = new UserModel({
-      login: req.body.login,
-      passwordHash: hashedPassword,
-      name: req.body.name,
-      surname: req.body.surname,
-      dateOfBirth: req.body.dateOfBirth,
-      city: req.body.city,
-      branch: req.body.branch,
-    })
-
-    const user = await doc.save()
-
-    serverLog(`Успешно создан пользователь с логином ${user.login}`)
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      TOKEN_KEY
-    )
-
-    res.json({ token: token })
-  } catch (err) {
-    serverError(err)
-    if (err?.keyPattern?.login === 1) {
-      return res.status(409).json({
-        errorMsg: 'Логин уже занят',
-      })
-    }
-    res.status(500).json({
-      errorMsg: 'Произошла ошибка во время регистрации',
-    })
-  }
-})
-
+app.post('/user/reg', registerValidation, UserController.reg)
 // user sign in
+app.post('/user/login', UserController.login)
+// show user profile
+app.get('/user/me', checkAuth, UserController.getMeInfo)
 
 // user edit his profile
 
@@ -101,5 +54,5 @@ app.listen(PORT, (err) => {
   if (err) {
     return console.log(err)
   }
-  console.log('Server: http://localhost:' + PORT)
+  serverLog('Server link: http://localhost:' + PORT)
 })
